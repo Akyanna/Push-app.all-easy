024264460d34f5f5c4bf20c1380784f7b1125a84
Akyanna <durkina.a.n@yandex.ru>
2024-07-25 13:28:23 +0300

    mobile version added

--- main.js
+++ main.js
@@ -1,34 +1,16 @@
-const movableElement = document.getElementById('movable-element');
-const movableButtonContent = document.getElementById('movable-footer-right');
+const navigation = document.querySelector('.navigation');
+const toggle = document.querySelector('.header__button');
 
-const header = document.querySelector('.header');
-const footer = document.getElementById('footer');
-const headerButton = document.getElementById('button');
+navigation.classList.add('navigation--closed');
 
-headerButton.addEventListener('click', () => {
-	if (movableButtonContent.classList.contains('footer-right--closed')) {
-		movableButtonContent.classList.remove('footer-right--closed');
-	} else {
-		movableButtonContent.classList.add('footer-right--closed');
-	}
-  });
+toggle.addEventListener('click', () => {
+  if (navigation.classList.contains('navigation--closed')) {
+    navigation.classList.remove('navigation--closed');
+    navigation.classList.add('navigation--opened');
+  } else {
+	navigation.classList.add('navigation--closed');
+	navigation.classList.remove('navigation--opened');
+  }
+});
 
-function handleMediaChange(e) {
-	console.log("handleMediaChange called")
-    if (e.matches) {
-		footer.appendChild(movableElement);
-		headerButton.appendChild(movableButtonContent);
-    }	else {
-		header.appendChild(movableElement);
-		footer.appendChild(movableButtonContent);
-	}
-}
 
-// Define the media query
-const mediaQuery = window.matchMedia('(max-width: 320px)');
-
-// Listen for changes to the media query
-mediaQuery.addEventListener('change', handleMediaChange);
-
-// Initial check
-handleMediaChange(mediaQuery);
